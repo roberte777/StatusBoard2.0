af65110592c283af0c3cef21142c3c0c36fc3bc8
Ethan <robewilkes@yahoo.com>
2021-11-21 21:03:49 -0600

    shows roles

--- pages/Admin/index.tsx
+++ pages/Admin/index.tsx
@@ -11,53 +11,47 @@ import {
 import {
   DataGrid,
   GridApi,
+  GridCellParams,
   GridCellValue,
   GridColDef,
-  GridRenderCellParams
+  GridRenderCellParams,
+  GridSelectionModel
 } from "@mui/x-data-grid";
-import TabPanel from "@/components/TabPanel";
-const generateButton = (role: "admin" | "employee") => {
-  return {
-    field: role,
-    headerName: `Add ${role.charAt(0).toUpperCase()}`,
-    sortable: false,
-    renderCell: (params: GridRenderCellParams) => {
-      const onClick = async (e: any) => {
-        e.stopPropagation(); // don't select this row after clicking
+import CheckIcon from "@mui/icons-material/Check";
+import ClearIcon from "@mui/icons-material/Clear";
 
-        const resp = await (
-          await fetch("/api/Roles", {
-            method: "POST",
-            body: JSON.stringify({
-              uid: params.row.uid,
-              role: role,
-              remove: false
-            })
-          })
-        ).json();
-      };
-    }
-  };
-};
-// const generateColumns: GridColDef[] = (
-//   type: "all" | "admin" | "employee" | "noRole"
-// ) => {
-//   return [
-//     { field: "email", headerName: "Email", width: 150 },
-//     { field: "displayName", headerName: "Name", width: 150 }
-//   ];
-// };
 const columns: GridColDef[] = [
-  { field: "email", headerName: "Email" },
-  { field: "displayName", headerName: "Name" },
-  { field: "employee", headerName: "Employee" },
-  { field: "admin", headername: "Admin" }
+  { field: "email", headerName: "Email", width: 300 },
+  { field: "displayName", headerName: "Name", width: 200 },
+  {
+    field: "employee",
+    headerName: "Employee",
+    type: "boolean",
+    renderCell: (params: GridCellParams) =>
+      params.value ? (
+        <CheckIcon color="success" />
+      ) : (
+        <ClearIcon color="warning" />
+      )
+  },
+  {
+    field: "admin",
+    headername: "Admin",
+    type: "boolean",
+    renderCell: (params: GridCellParams) =>
+      params.value ? (
+        <CheckIcon color="success" />
+      ) : (
+        <ClearIcon color="warning" />
+      )
+  }
 ];
 
 export default function index() {
   const [users, setUsers] = useState<[] | User[]>([]);
   const [dataLoading, setDataLoading] = useState(false);
   const [value, setValue] = React.useState(0);
+  const [selectionModel, setSelectionModel] = useState<GridSelectionModel>([]);
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
@@ -85,37 +79,32 @@ export default function index() {
   }
   return (
     <div>
-      {users.map((e) => (
-        <div key={e.uid}>{e.displayName}</div>
-      ))}
-      <Box sx={{ width: "100%" }}>
-        {/* <Box sx={{ borderBottom: 1, borderColor: "divider" }}> */}
-        {/* <Tabs
-            value={value}
-            onChange={handleChange}
-            aria-label="basic tabs example"
-          >
-            <Tab label="All" {...a11yProps(0)} />
-            <Tab label="Employee" {...a11yProps(1)} />
-            <Tab label="Admin" {...a11yProps(2)} />
-            <Tab label="No Role" {...a11yProps(3)} />
-          </Tabs> */}
-        {/* </Box> */}
-        {/* <TabPanel value={value} index={0}> */}
-        <DataGrid
-          columns={columns}
-          rows={users.map((user, idx) => {
-            return {
-              ...user,
-              id: idx,
-              employee: user.customClaims?.employee,
-              admin: user.customClaims?.admin
-            };
-          })}
-          // checkboxSelection
-        />
-        {/* </TabPanel> */}
-      </Box>
+      <div
+        style={{
+          display: "flex",
+          height: "50vh",
+          width: "100%"
+        }}
+      >
+        <div style={{ flexGrow: 1 }}>
+          <DataGrid
+            onSelectionModelChange={(newSelectionModel) => {
+              setSelectionModel(newSelectionModel);
+            }}
+            selectionModel={selectionModel}
+            columns={columns}
+            rows={users.map((user, idx) => {
+              return {
+                ...user,
+                id: idx,
+                employee: user.customClaims?.employee,
+                admin: user.customClaims?.admin
+              };
+            })}
+          />
+        </div>
+      </div>
+      {selectionModel.map((e) => JSON.stringify(e))}
     </div>
   );
 }
