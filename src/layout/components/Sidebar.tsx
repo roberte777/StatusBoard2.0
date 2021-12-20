170c4538a7ddf4173d0c6871d796da6300dc235f
Ethan <robewilkes@yahoo.com>
2021-12-19 23:18:00 -0600

    made website title smaller

--- src/layout/components/Sidebar.tsx
+++ src/layout/components/Sidebar.tsx
@@ -1,58 +1,54 @@
 import React, { Dispatch, SetStateAction } from "react";
 import Link from "next/link";
 import {
-    Toolbar,
-    Divider,
-    List,
-    ListItem,
-    ListItemIcon,
-    ListItemText,
-    IconButton
+  Toolbar,
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  IconButton,
 } from "@mui/material";
 import sidebarConfig from "../../constants/sidebarConfig";
 import { ChevronLeft } from "@mui/icons-material";
 interface Section {
-    title: string;
-    icon: any;
-    link: string;
+  title: string;
+  icon: any;
+  link: string;
 }
 export default function sidebar({
-    setDrawerOpen
+  setDrawerOpen,
 }: {
-    setDrawerOpen: Dispatch<SetStateAction<boolean>>;
+  setDrawerOpen: Dispatch<SetStateAction<boolean>>;
 }) {
-    return (
-        <div>
-            <Toolbar sx={{ justifyContent: "center", maxHeight: "100%" }}>
-                <h2 style={{ maxHeight: "100%" }}>
-                    {process.env.NEXT_PUBLIC_PAGE_NAME}
-                </h2>
-                <IconButton onClick={() => setDrawerOpen(false)}>
-                    <ChevronLeft />
-                </IconButton>
-            </Toolbar>
-            <Divider />
-            {sidebarConfig.map((section: Section[], idx: number) => (
-                <nav key={idx}>
-                    <List key={idx}>
-                        {section.map((subsection: Section, idx: number) => (
-                            <Link href={subsection.link} key={idx}>
-                                <a>
-                                    <ListItem button key={subsection.title}>
-                                        <ListItemIcon>
-                                            {subsection.icon}
-                                        </ListItemIcon>
-                                        <ListItemText
-                                            primary={subsection.title}
-                                        />
-                                    </ListItem>
-                                </a>
-                            </Link>
-                        ))}
-                    </List>
-                    <Divider />
-                </nav>
+  return (
+    <div>
+      <Toolbar sx={{ justifyContent: "center", maxHeight: "100%" }}>
+        <h3 style={{ maxHeight: "100%" }}>
+          {process.env.NEXT_PUBLIC_PAGE_NAME}
+        </h3>
+        <IconButton onClick={() => setDrawerOpen(false)}>
+          <ChevronLeft />
+        </IconButton>
+      </Toolbar>
+      <Divider />
+      {sidebarConfig.map((section: Section[], idx: number) => (
+        <nav key={idx}>
+          <List key={idx}>
+            {section.map((subsection: Section, idx: number) => (
+              <Link href={subsection.link} key={idx}>
+                <a>
+                  <ListItem button key={subsection.title}>
+                    <ListItemIcon>{subsection.icon}</ListItemIcon>
+                    <ListItemText primary={subsection.title} />
+                  </ListItem>
+                </a>
+              </Link>
             ))}
-        </div>
-    );
+          </List>
+          <Divider />
+        </nav>
+      ))}
+    </div>
+  );
 }
